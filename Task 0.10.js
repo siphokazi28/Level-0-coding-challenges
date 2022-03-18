d3780c81c302cd3bf55abca7b87999440bb864ff
Siphokazi <celinetsiko@gmail.com>
2022-03-18 16:50:44 +0200

    made changes to code

--- Task 0.10.js
+++ Task 0.10.js
@@ -1,15 +1,3 @@
-/* Make a function that takes two strings as input, and outputs the common characters/letters that they share.
-
-For example, Input: ‘house’, ‘computers’ . Output: ‘Common letters: o, u, e, s’)*/
-/*let word1 = "wholesome";
-let word2 = "bookworm";
-let count = 0;
-function commonLetters(word1, word2) {
-    for (let i in word1)
-    word2.match(word1[i])?match++:false;
-    console.log(match)
-}
-commonLetters(word1, word2);*/
 let word1 = "wholesome";
 let word2 = "bookworm";
 function commonLetters(word1, word2){
