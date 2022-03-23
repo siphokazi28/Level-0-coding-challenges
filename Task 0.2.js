509d38b741fe2fd262c6139ac5f95d76869ad806
Siphokazi <celinetsiko@gmail.com>
2022-03-23 14:11:54 +0200

    declared with const

--- Task 0.2.js
+++ Task 0.2.js
@@ -1,10 +1,10 @@
- let x = (1 + 1) * 2;
- let y = (1 + 1) * 2;
- let z = 1 + (1 * 2);
- let a = (1 + 1) * (2 /2);
- let b = (1 + 1 * 2) / 2;
- console.log(x);
- console.log(y);
- console.log(z);
- console.log(a);
- console.log(b);
+const x = (1 + 1) * 2
+const y = (1 + 1) * 2
+const z = 1 + 1 * 2
+const a = (1 + 1) * (2 / 2)
+const b = (1 + 1 * 2) / 2
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(a)
+console.log(b)
