cd1d839f4d68a793c11705874ef366539a5103a7
Siphokazi <celinetsiko@gmail.com>
2022-03-23 17:11:03 +0200

    fixed let mistake

--- Task 0.6.js
+++ Task 0.6.js
@@ -1,9 +1,6 @@
-let number1 = 12;
-let number2 = 56;
-let number3 = 23;
-function maxNumber(number1, number2, number3){
-maxNumber = Math.max(number1, number2, number3);
-return maxNumber;
-} 
-let result = maxNumber(number1, number2, number3);
-console.log(maxNumber);
+function maxNumber(number1, number2, number3) {
+  maxNumber = Math.max(number1, number2, number3)
+  return maxNumber
+}
+let result = maxNumber(29, 100, 70)
+console.log(result)
