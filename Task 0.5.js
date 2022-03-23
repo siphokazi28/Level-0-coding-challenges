5561e4fd3db635bb7d001465bfb90657785ea41f
Siphokazi <celinetsiko@gmail.com>
2022-03-23 17:03:58 +0200

    used const instead of let

--- Task 0.5.js
+++ Task 0.5.js
@@ -1,10 +1,9 @@
-let side1 = 14;
-let side2 = 16;
-let side3 = 20;
 function areaOfTriangle(side1, side2, side3) {
-  let perimeter = (side1 + side2 + side3)/2;
-  let areaOfTriangle = Math.sqrt(((perimeter - side1)*(perimeter - side2)*(perimeter - side3)*perimeter));
-  return areaOfTriangle;
+  const perimeter = (side1 + side2 + side3) / 2
+  const areaOfTriangle = Math.sqrt(
+    (perimeter - side1) * (perimeter - side2) * (perimeter - side3) * perimeter,
+  )
+  return areaOfTriangle
 }
-let result = areaOfTriangle(side1, side2, side3);
-console.log(result);
+let result = areaOfTriangle(14, 16, 20)
+console.log(result)
