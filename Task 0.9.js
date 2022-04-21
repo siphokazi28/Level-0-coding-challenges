8b245a4b9a1ad8507fc3bf53d57e710cd4fd0151
Siphokazi <celinetsiko@gmail.com>
2022-04-21 07:23:38 +0200

    made new changes

--- Task 0.9.js
+++ Task 0.9.js
@@ -1,5 +1,17 @@
-let word = "welcomE";
-function findVowel(word) {
-    return word.match(/[aeiouAEIOU]/gi);
+function findVowel(str) {
+  for (i = 0; i < str.length; i++) {
+    if (
+      str[i] == 'a' ||
+      str[i] == 'e' ||
+      str[i] == 'i' ||
+      str[i] == 'o' ||
+      str[i] == 'u'
+    ) {
+      console.log(str[i])
+    }
+  }
 }
-    console.log(findVowel(word));
+let word = 'Umuzi'
+let word2 = word.toLowerCase()
+let word3 = Array.from(new Set(word2)).join('')
+findVowel(word3)
